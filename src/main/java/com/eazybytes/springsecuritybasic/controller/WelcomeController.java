7e988ddab861dda054580603005defedf6884d0e
Yohanes Wahyu Hardi Pratama <yohanes_wahyu@hotmail.com>
2023-11-20 20:05:09 +0700

    first commit

---
+++ src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.eazybytes.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String sayWelcome() {
+        System.out.println("Welcome to Spring Application with Security");
+        return "Welcome to Spring Application with Security";
+    }
+
+}
