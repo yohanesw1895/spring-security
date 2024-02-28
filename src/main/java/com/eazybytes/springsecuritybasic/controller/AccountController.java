258c5953a793a271091d6ef587faf3d1b5275eb9
Yohanes Wahyu Hardi Pratama <yohanes_wahyu@hotmail.com>
2024-02-28 17:30:31 +0700

    get username login

--- src/main/java/com/eazybytes/springsecuritybasic/controller/AccountController.java
+++ src/main/java/com/eazybytes/springsecuritybasic/controller/AccountController.java
@@ -2,8 +2,13 @@ package com.eazybytes.springsecuritybasic.controller;
 
 import com.eazybytes.springsecuritybasic.model.Customer;
 import com.eazybytes.springsecuritybasic.repository.CustomerRepository;
+import org.apache.catalina.security.SecurityUtil;
+import org.keycloak.KeycloakPrincipal;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +26,23 @@ public class AccountController {
             value = "/myAccount",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Customer> getMyAccount(@RequestParam String email) throws Exception {
+    public ResponseEntity<Customer> getMyAccount(@RequestParam String email, Authentication authentication) throws Exception {
 
-        System.out.println("Here are the account details from the DB");
+        System.out.println("Principal class: " + authentication.getPrincipal().getClass().getName());
+
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        // Extract information from JWT claims
+        String username = jwt.getClaimAsString("preferred_username");
+        // Other claims...
+
+
+//        KeycloakPrincipal<?> keycloakPrincipal = (KeycloakPrincipal<?>) authentication.getPrincipal();
+//
+//        // You can now access user information from the KeycloakPrincipal object
+//        // For example, to get the username (subject ID in Keycloak terms)
+//        String username = keycloakPrincipal.getName();
+
+        System.out.println("Here are the account details from the DB, username : " + username);
         return ResponseEntity.ok(
                 customerRepository.findOneByEmail(email)
                         .orElseThrow(() -> new Exception("data not found"))
